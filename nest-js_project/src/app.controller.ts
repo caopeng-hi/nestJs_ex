ddb1aa511872f35dbc425c3a9eb21297a2bd117f
caopeng <caopeng_404@163.com>
2025-08-21 21:59:30 +0800

    集成redis

--- nest-js_project/src/app.controller.ts
+++ nest-js_project/src/app.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { InjectRedis } from '@nestjs-modules/ioredis';
+import { Controller, Get, Query } from '@nestjs/common';
+import Redis from 'ioredis';
 
 @Controller()
 export class AppController {
-  constructor() {}
+  constructor(
+    @InjectRedis() private readonly redis:Redis
+  ) {}
 
   @Get()
-  getHello(): string {
+  async getHello(@Query('token' ) token) {
+    const res = await this.redis.get('token')
+    console.log(res);
+
+    await this.redis.set('token',token ||'caopeng','EX',60)
+    
     return 'hello';
   }
 }
--- nest-js_project/src/app.module.ts
+++ nest-js_project/src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from './common/config/config.module';
 import { LogsModule } from './common/logs_log/logs.module';
+import { RedisModule } from '@nestjs-modules/ioredis';
 
 
 @Module({
-  imports:[ConfigModule, LogsModule],
+  imports: [ConfigModule, LogsModule, RedisModule.forRoot({
+    type: 'single',
+    url: 'redis://localhost:6379',
+    options: {
+      password:'example'
+    }
+    
+  })],
   controllers: [AppController],
   providers: [],
 })
